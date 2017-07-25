40b26e1b943bbcb3a4d66c45a2fd003b36108d57
Christopher Hernandez <christohernandez011@gmail.com>
2017-07-25 14:00:41 -0400

    Various changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function isElementInViewport(el) {
   return (
     rect.top >= 0 &&
     rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.bottom <= (window.innerHeight + 300 || document.documentElement.clientHeight + 300) &&
     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
   );
 }
---
+++ paradise/paradiserealestate.js
@@ -0,0 +1,24 @@
+function listFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+function myFunction() {
+    var x = document.getElementById('square1');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
