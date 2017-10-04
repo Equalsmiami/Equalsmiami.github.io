529faf13134d7b64a2fa3468fee4987671d89f28
Equalsmiami <christohernandez011@gmail.com>
2017-10-04 15:45:43 -0400

    More Corrections to CSS.

--- index.js
+++ index.js
@@ -22,29 +22,3 @@ function callbackFunc() {
 
 window.addEventListener("load", callbackFunc);
 window.addEventListener("scroll", callbackFunc);
-
-
-/**
- * This was built using the scrollie jQuery Plugin
- * https://github.com/Funsella/jquery-scrollie
- */
-
-
-$( window ).ready(function() {
-
-    var wHeight = $(window).height();
-
-    $('.slide')
-      .height(wHeight)
-      .scrollie({
-        scrollOffset : -50,
-        scrollingInView : function(elem) {
-
-          var bgColor = elem.data('background');
-
-          $('body').css('background-color', bgColor);
-
-        }
-      });
-
-  });
